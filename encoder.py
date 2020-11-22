d527add2f56dee6cc6bc3bb1de23a94c815ae3ee
Ivan Lopes <ivan.lopes@live.fr>
2020-11-22 14:01:05 +0100

    Return list of unknown songs for playlist encoding

--- encoder.py
+++ encoder.py
@@ -16,28 +16,34 @@ class PlaylistEncoder():
         self.mapping = {tid: i for i, (tid, *_) in enumerate(tracks)}
         self.accumulate = accumulate
 
+    # def __call__(self, tids):
+    #     track_indices = [self.mapping[tid] for tid in tids if tid in self.mapping]
+    #     encoding = np.zeros(len(self))
+    #     for index, listens in zip(*np.unique(track_indices, return_counts=True)):
+    #         encoding[index] = listens if self.accumulate else 1
+    #     return encoding
+
     def __call__(self, tids):
-        track_indices = [self.mapping[tid] for tid in tids if tid in self.mapping]
-        encoding = np.zeros(len(self))
-        for index, listens in zip(*np.unique(track_indices, return_counts=True)):
-            encoding[index] = listens if self.accumulate else 1
-        return encoding
+        return [self.mapping[tid] for tid in tids if tid in self.mapping]
 
     def __len__(self):
         return len(self.mapping)
 
 
 class MelEncoder():
-    def __init__(self, fp, mean, std, length):
+    def __init__(self, fp, mean, std, length, min=0, max=1, loader=None):
         self.fp = fp
         self.length = length
+        self.min = min
+        self.max = max
         self.mean = mean
         self.std = std
 
     def __call__(self, tid):
         mel = np.load(os.sep.join((self.fp, tid+'.npy')))
-        tiled = np.tile(mel, (1, 1 + self.length // mel.shape[1]))
-        normalized = (tiled[:,:self.length] - self.mean) / self.std
+        tiled = np.tile(mel, (1, 1 + self.length // mel.shape[1]))[:,:self.length]
+        mapped = (tiled - self.min)/(self.max - self.min)
+        normalized = (mapped - self.mean)/self.std
         return normalized
 
     def __len__(self):
